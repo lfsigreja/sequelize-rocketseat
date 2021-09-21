bcbaa286f7151c502613096a00b20e5585110144
Luis Igreja <lfsigreja@gmail.com>
2021-09-21 15:22:25 -0300

    adding address route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 const UserController = require('./Controllers/UserController');
+const AddressController = require('./Controllers/AddressController');
 
 const routes = express.Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
+
+routes.post('/users/:user_id/addresses', AddressController.store);
 
 module.exports = routes;
