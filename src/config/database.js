ec6f5246f2a4fc065eea7a99ef9636e6cb89aa6b
Luis Igreja <lfsigreja@gmail.com>
2021-09-20 23:23:15 -0300

    sequelize + postgres study

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'lkz1',
+  password: '1234',
+  database: 'agendamento',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
