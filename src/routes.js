abeb28ca8ab42ccde7706a49f03921ee5cc119c3
Luis Igreja <lfsigreja@gmail.com>
2021-09-21 19:28:30 -0300

    adding 3 tables relation

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const UserController = require('./Controllers/UserController');
 const AddressController = require('./Controllers/AddressController');
+const TechController = require('./Controllers/TechController');
 
 const routes = express.Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
 
+routes.get('/users/:user_id/addresses', AddressController.index);
 routes.post('/users/:user_id/addresses', AddressController.store);
+
+routes.get('/users/:user_id/techs', TechController.index);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
 
 module.exports = routes;
